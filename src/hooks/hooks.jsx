c8c8de42d77735760dc9a75a2b0f3adf38bc4978
FestimsRepo <102750865+FestimP@users.noreply.github.com>
2022-07-25 15:29:09 +0200

    "finished hooks"

---
+++ src/hooks/hooks.jsx
@@ -0,0 +1,28 @@
+import { display } from "@mui/system";
+import { useState } from "react";
+import "./hoosk.css";
+import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowUp } from "react-icons/io";
+
+export default function FunkApp() {
+
+//   const [isLoading, setIsLoading] = useState(false);
+
+//   const toggleIsLoading = () => {
+
+//     setIsLoading(current => !current);
+//   };
+
+const [inSide,outSide] = useState(false);
+const toggleInSide = () => {
+    outSide(current => !current);
+}
+  return (
+    <div className="Sups">
+      <button className="btnS" onClick={toggleInSide}><p>Why park a domain name in Parkname ?</p><IoIosArrowDown className={inSide ? "icon2" : "icon1"}/></button>
+       <p className={inSide ? "us" : "bus"}>
+        Park is the leading industry standard for domain name parking and monetization servicies .We offer a wide variety of sefvices to help you achive succses
+       </p> 
+    </div>
+  );
+}
